5c479e404e4d0c8e32bafa05f0fbcac74a230d30
Kevin Ruble <keruble@dekumi.com>
2018-12-23 06:27:40 -0800

    Run params through conversion to decimal

--- idview/idview.lua
+++ idview/idview.lua
@@ -121,7 +121,7 @@ function get_params(params_string)
     params[i + 1] = string.sub(params_string, (i*8)+1, (i*8) + 8);
   end
   for _,v in ipairs(params) do
-    final_param_string = final_param_string .. v .. ", ";
+    final_param_string = final_param_string .. byte_string_to_int(v) .. ", ";
   end
   return final_param_string;
 end
